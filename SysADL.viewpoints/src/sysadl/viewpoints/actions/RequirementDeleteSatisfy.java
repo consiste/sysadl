df94c6a1cc9c4d2c181433d19eb6289b04779496
eduardo <eduafsilva@gmail.com>
2018-01-25 18:49:13 +0100

    Implemented RequirementDeleteSatisfy.java to delete imported elements in requirements diagram

--- SysADL.viewpoints/src/sysadl/viewpoints/actions/RequirementDeleteSatisfy.java
+++ SysADL.viewpoints/src/sysadl/viewpoints/actions/RequirementDeleteSatisfy.java
@@ -7,18 +7,31 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.sirius.business.api.action.AbstractExternalJavaAction;
 import org.eclipse.sirius.tools.api.ui.IExternalJavaAction;
 
+import sysADL_Sintax.ElementDef;
+import sysADL_Sintax.Model;
+
 public class RequirementDeleteSatisfy extends AbstractExternalJavaAction {
 
 	@Override
 	public boolean canExecute(Collection<? extends EObject> arg0) {
-		// TODO Auto-generated method stub
-		return false;
+		for (EObject e : arg0) {
+			if (!(e instanceof ElementDef)) {
+				return false;
+			}
+		}
+		return true; 
 	}
 
 	@Override
 	public void execute(Collection<? extends EObject> arg0, Map<String, Object> arg1) {
-		// TODO Auto-generated method stub
-		
+		for (EObject arg : arg0) {
+			ElementDef e = (ElementDef) arg;
+			// remove from requirement satisfy list
+			e.getSatisfies().getSatisfiedBy().remove(e);
+			// add back to the model involved elements list, so the element will not vanish
+			Model m = getOptionalParameter(arg1, "model", Model.class);
+			m.getInvolvedElements().add(e);
+		}
 	}
 
 }

e5b0d68e134c327a28b69c623abe1077c1aa5ca1
eduardo <eduafsilva@gmail.com>
2018-03-09 20:08:02 +0100

    Added UiActivityPinInput class and ObjectValue
    UiActivityPinInput is a user-interface class that requests values for the inputs Pins, ObjectValue is to allow the tool to differ empty values and null values
    Added RequirementDeleteComposition.java, not implemented yet

---
+++ SysADL.viewpoints/src/sysadl/viewpoints/actions/RequirementDeleteComposition.java
@@ -0,0 +1,23 @@
+package sysadl.viewpoints.actions;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.sirius.tools.api.ui.IExternalJavaAction;
+
+public class RequirementDeleteComposition implements IExternalJavaAction {
+
+	@Override
+	public boolean canExecute(Collection<? extends EObject> arg0) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public void execute(Collection<? extends EObject> arg0, Map<String, Object> arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ org.sysadl.aspects/src/org/sysadl/execution/ui/UiActivityPinInput.java
@@ -0,0 +1,51 @@
+package org.sysadl.execution.ui;
+
+import sysADL_Sintax.ActivityDef;
+import sysADL_Sintax.Pin;
+
+/**
+ * This class is responsible for capturing Input values and presenting Output values 
+ * for Pin in an Activity
+ * Extend it to implement its methods 
+ * @author Eduardo
+ *
+ */
+public abstract class UiActivityPinInput {
+	/**
+	 * Save the activity for internal use
+	 * Use activity.getInParameters() to retrieve a list of Pins
+	 */
+	private ActivityDef activity;
+	
+	public UiActivityPinInput(ActivityDef ac) {
+		this.activity = ac;
+	}
+	
+	/**
+	 * Requests input from user
+	 * 
+	 * Set all empty values to ObjectValue.EMPTY
+	 * Empty is different from null, null is value of TypeDef Void
+	 * 
+	 * Default TypeDefs and its Java types:
+	 * <SysADL Type> = <Java Class>
+	 * TypeDef Void = null
+	 * TypeDef Int = Integer
+	 * TypeDef Real = Double
+	 * TypeDef String = String
+	 * TypeDef Boolean = Boolean
+	 * 
+	 * Check TypeDef of each Pin and request data according to it
+	 * 
+	 * @return true if user provided values for all pins, false otherwise
+	 * Note: The user might not provide all inputs and it will NOT be an error
+	 */
+	public abstract boolean requestInput();
+	
+	/**
+	 * 
+	 * @param target pin
+	 * @return value provided for Pin
+	 */
+	public abstract Object inputValueOf(Pin target);
+}

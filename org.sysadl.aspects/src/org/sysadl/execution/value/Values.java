0da9a2e9fd60e8cd576e94dec8f1806b529834b0
eduardo <eduafsilva@gmail.com>
2018-03-26 14:30:23 +0200

    Adjusted type VOID
    Void datastores are constantly fed with VOID values

--- org.sysadl.aspects/src/org/sysadl/execution/value/Values.java
+++ org.sysadl.aspects/src/org/sysadl/execution/value/Values.java
@@ -1,9 +1,23 @@
 package org.sysadl.execution.value;
 
+import sysADL_Sintax.TypeDef;
+
 public class Values {
-	public static Values VOID = new Values();
+	private int id;
 	
+	private static int VOID_VALUE = 0;
+	
+	public static Values VOID = new Values(0);
+	
+	private Values(int i) {
+		this.id = i;
+	}
+
 	public static boolean isVoid(Object o) {
-		return (o == Values.VOID);
+		return (o instanceof Values) && ((Values) o).id==VOID_VALUE;
+	}
+	
+	public static boolean isVoidType(TypeDef t) {
+		return t.getName().equals("Void");
 	}
 }

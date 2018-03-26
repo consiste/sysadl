7c5123879fc12a11606bbad777726273c1857931
eduardo <eduafsilva@gmail.com>
2018-03-26 10:55:45 +0200

    Added getMessage to InputUiException

--- org.sysadl.aspects/src/org/sysadl/execution/ui/InputUiException.java
+++ org.sysadl.aspects/src/org/sysadl/execution/ui/InputUiException.java
@@ -1,5 +1,6 @@
 package org.sysadl.execution.ui;
 
+import sysADL_Sintax.ForControl;
 import sysADL_Sintax.Pin;
 
 public class InputUiException extends Exception {
@@ -9,12 +10,23 @@ public class InputUiException extends Exception {
 	 */
 	private static final long serialVersionUID = 6391387909965747296L;
 	
+	private String message;
 	/**
 	 * 
 	 * @param pins array with pins which provided values were inadequate
 	 */
 	public InputUiException(Pin[] pins) {
-		
+		super();
+		message = "";
+		for (Pin p : pins) {
+			if (!message.isEmpty()) message+="\n";
+			message += "(Pin)" +p.getName()+" : "+p.getDefinition().getName();
+		}
+	}
+	
+	@Override
+	public String getMessage() {
+		return message;
 	}
 
 }

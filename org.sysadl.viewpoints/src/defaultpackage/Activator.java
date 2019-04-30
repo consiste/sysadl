eb74dee8bb5cbf5d6566ad39fd196ac805424066
Eduardo Silva <eduafsilva@gmail.com>
2019-04-30 15:50:14 -0300

    Fixed plugin ID and Activator

--- org.sysadl.viewpoints/src/defaultpackage/Activator.java
+++ org.sysadl.viewpoints/src/defaultpackage/Activator.java
@@ -13,7 +13,7 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator extends AbstractUIPlugin {
     // The plug-in ID
-    public static final String PLUGIN_ID = "SysADL.viewpoints";
+    public static final String PLUGIN_ID = "org.sysadl.viewpoints";
 
     // The shared instance
     private static Activator plugin;

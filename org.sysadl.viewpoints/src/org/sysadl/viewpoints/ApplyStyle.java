d69efaf4342643b688881777471344dc103325b5
Eduardo Silva <eduafsilva@gmail.com>
2019-05-28 15:25:53 -0300

    Created new external java action to apply style to an element

---
+++ org.sysadl.viewpoints/src/org/sysadl/viewpoints/ApplyStyle.java
@@ -0,0 +1,27 @@
+package org.sysadl.viewpoints;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.sirius.tools.api.ui.IExternalJavaAction;
+
+public class ApplyStyle implements IExternalJavaAction {
+
+	public ApplyStyle() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean canExecute(Collection<? extends EObject> arg0) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public void execute(Collection<? extends EObject> arg0, Map<String, Object> arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

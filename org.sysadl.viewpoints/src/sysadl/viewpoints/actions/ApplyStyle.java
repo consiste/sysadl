f97a246ccc70ef5d21d97eac5ab117158f68a588
Eduardo Silva <eduafsilva@gmail.com>
2019-05-30 16:41:13 -0300

    Fixed function to apply style to any element in the BDD

--- org.sysadl.viewpoints/src/sysadl/viewpoints/actions/ApplyStyle.java
+++ org.sysadl.viewpoints/src/sysadl/viewpoints/actions/ApplyStyle.java
@@ -5,6 +5,13 @@ import java.util.Map;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.sirius.tools.api.ui.IExternalJavaAction;
+import org.sysadl.AbstractComponentDef;
+import org.sysadl.AbstractConnectorDef;
+import org.sysadl.AbstractDef;
+import org.sysadl.AbstractPortUse;
+import org.sysadl.ComponentDef;
+import org.sysadl.ConnectorDef;
+import org.sysadl.PortUse;
 
 public class ApplyStyle implements IExternalJavaAction {
 
@@ -14,14 +21,24 @@ public class ApplyStyle implements IExternalJavaAction {
 
 	@Override
 	public boolean canExecute(Collection<? extends EObject> arg0) {
-		// TODO Auto-generated method stub
+		// can always execute
 		return true;
 	}
 
 	@Override
 	public void execute(Collection<? extends EObject> arg0, Map<String, Object> arg1) {
-		// TODO Auto-generated method stub
-		
+		Object element = arg1.get("style");
+		for (EObject obj : arg0) {
+			if (obj instanceof ComponentDef) {
+				((ComponentDef) obj).setAbstractComponent((AbstractComponentDef) element);
+			}
+			else if (obj instanceof ConnectorDef) {
+				((ConnectorDef) obj).setAbstractConnector((AbstractConnectorDef) element);
+			}
+			else if (obj instanceof PortUse) {
+				((PortUse) obj).setAbstractPort((AbstractPortUse) element);
+			}
+		}
 	}
 
 }

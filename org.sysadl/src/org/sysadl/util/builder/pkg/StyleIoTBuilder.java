906676686107956524f372f311f02716d93b5c9d
Lidiane Santos <diane_lid@hotmail.com>
2019-10-04 21:22:17 -0300

    Changed StyleIoTBuilder

--- org.sysadl/src/org/sysadl/util/builder/pkg/StyleIoTBuilder.java
+++ org.sysadl/src/org/sysadl/util/builder/pkg/StyleIoTBuilder.java
@@ -5,6 +5,7 @@ import org.sysadl.AbstractComponentDef;
 import org.sysadl.AbstractConnectorDef;
 import org.sysadl.AbstractFlow;
 import org.sysadl.AbstractPortUse;
+import org.sysadl.FlowProperty;
 import org.sysadl.Invariant;
 import org.sysadl.Style;
 import org.sysadl.SysADLFactory;
@@ -15,116 +16,205 @@ public class StyleIoTBuilder extends ElementBuilder {
 	public Style build() {
 		Style IoTStyle = SysADLFactory.eINSTANCE.createStyle();
 		IoTStyle.setName("IoTStyle");
-		
-		// ports
-		AbstractPortUse sensorOPT = SysADLFactory.eINSTANCE.createAbstractPortUse();
-		sensorOPT.setName("SensorOPT");
-		AbstractPortUse sensorIPT = SysADLFactory.eINSTANCE.createAbstractPortUse();
-		sensorIPT.setName("SensorIPT");
-		AbstractPortUse actuatorOPT = SysADLFactory.eINSTANCE.createAbstractPortUse();
-		actuatorOPT.setName("ActuatorOPT");
-		AbstractPortUse actuatorIPT = SysADLFactory.eINSTANCE.createAbstractPortUse();
-		actuatorIPT.setName("ActuatorIPT");
-		AbstractPortUse deviceOPT = SysADLFactory.eINSTANCE.createAbstractPortUse();
-		deviceOPT.setName("DeviceOPT");
-		AbstractPortUse deviceIPT = SysADLFactory.eINSTANCE.createAbstractPortUse();
-		deviceIPT.setName("DeviceIPT");
-		AbstractPortUse dataOPT = SysADLFactory.eINSTANCE.createAbstractPortUse();
-		dataOPT.setName("DataOPT");
-		AbstractPortUse dataIPT = SysADLFactory.eINSTANCE.createAbstractPortUse();
-		dataIPT.setName("DataIPT");
-		AbstractPortUse serviceOPT = SysADLFactory.eINSTANCE.createAbstractPortUse();
-		serviceOPT.setName("ServiceOPT");
-		AbstractPortUse serviceIPT = SysADLFactory.eINSTANCE.createAbstractPortUse();
-		serviceIPT.setName("ServiceIPT");
-		
+
 		// components
 		AbstractComponentDef sensor = SysADLFactory.eINSTANCE.createAbstractComponentDef();
 		sensor.setName("SensorCP");
+		sensor.setLowerBound(1);
+		sensor.setUpperBound(-1);
+		AbstractPortUse sensorOPT = SysADLFactory.eINSTANCE.createAbstractPortUse();
+		sensorOPT.setName("SensorOPT");
+		sensorOPT.setFlowProperty(FlowProperty.OUT);
+		sensorOPT.setLowerBound(1);
+		sensorOPT.setUpperBound(-1);
 		sensor.getPorts().add(sensorOPT);
 		
 		AbstractComponentDef actuator = SysADLFactory.eINSTANCE.createAbstractComponentDef();
 		actuator.setName("ActuatorCP");
+		actuator.setLowerBound(0);
+		actuator.setUpperBound(-1);
+		AbstractPortUse actuatorIPT = SysADLFactory.eINSTANCE.createAbstractPortUse();
+		actuatorIPT.setName("ActuatorIPT");
+		actuatorIPT.setFlowProperty(FlowProperty.IN);
+		actuatorIPT.setLowerBound(1);
+		actuatorIPT.setUpperBound(-1);
 		actuator.getPorts().add(actuatorIPT);
-		
+				
 		AbstractComponentDef datastore = SysADLFactory.eINSTANCE.createAbstractComponentDef();
 		datastore.setName("DataStoreCP");
-		datastore.getPorts().add(dataIPT);
+		datastore.setLowerBound(1);
+		datastore.setUpperBound(-1);
+		AbstractPortUse dataOPT = SysADLFactory.eINSTANCE.createAbstractPortUse();
+		dataOPT.setName("DataOPT");
+		dataOPT.setFlowProperty(FlowProperty.OUT);
+		dataOPT.setLowerBound(1);
+		dataOPT.setUpperBound(-1);
 		datastore.getPorts().add(dataOPT);
-		
+		AbstractPortUse dataIPT = SysADLFactory.eINSTANCE.createAbstractPortUse();
+		dataIPT.setName("DataIPT");
+		dataIPT.setFlowProperty(FlowProperty.IN);
+		dataIPT.setLowerBound(1);
+		dataIPT.setUpperBound(-1);
+		datastore.getPorts().add(dataIPT);
+				
 		AbstractComponentDef controller = SysADLFactory.eINSTANCE.createAbstractComponentDef();
 		controller.setName("ControllerCP");
+		controller.setLowerBound(0);
+		controller.setUpperBound(-1);
+		AbstractPortUse sensorIPT = SysADLFactory.eINSTANCE.createAbstractPortUse();
+		sensorIPT.setName("SensorIPT");
+		sensorIPT.setFlowProperty(FlowProperty.IN);
+		sensorIPT.setLowerBound(0);
+		sensorIPT.setUpperBound(-1);
 		controller.getPorts().add(sensorIPT);
+		AbstractPortUse actuatorOPT = SysADLFactory.eINSTANCE.createAbstractPortUse();
+		actuatorOPT.setName("ActuatorOPT");
+		actuatorOPT.setFlowProperty(FlowProperty.OUT);
+		actuatorOPT.setLowerBound(0);
+		actuatorOPT.setUpperBound(-1);
 		controller.getPorts().add(actuatorOPT);
 		
 		AbstractComponentDef gateway = SysADLFactory.eINSTANCE.createAbstractComponentDef();
 		gateway.setName("GatewayCP");
-		gateway.getPorts().add(deviceIPT);
-		gateway.getPorts().add(serviceIPT);
-		gateway.getPorts().add(serviceOPT);
+		gateway.setLowerBound(0);
+		gateway.setUpperBound(-1);
 		gateway.getComposition().add(datastore);
-		
+		AbstractPortUse deviceIPT = SysADLFactory.eINSTANCE.createAbstractPortUse();
+		deviceIPT.setName("DeviceIPT");
+		deviceIPT.setFlowProperty(FlowProperty.IN);
+		deviceIPT.setLowerBound(1);
+		deviceIPT.setUpperBound(-1);
+		gateway.getPorts().add(deviceIPT);
+		AbstractPortUse serviceOPT = SysADLFactory.eINSTANCE.createAbstractPortUse();
+		serviceOPT.setName("ServiceOPT");
+		serviceOPT.setFlowProperty(FlowProperty.OUT);
+		serviceOPT.setLowerBound(1);
+		serviceOPT.setUpperBound(-1);
+		gateway.getPorts().add(serviceOPT);
+		AbstractPortUse serviceIPT = SysADLFactory.eINSTANCE.createAbstractPortUse();
+		serviceIPT.setName("ServiceIPT");
+		serviceIPT.setFlowProperty(FlowProperty.IN);
+		serviceIPT.setLowerBound(0);
+		serviceIPT.setUpperBound(-1);
+		gateway.getPorts().add(serviceIPT);
+						
 		AbstractComponentDef device = SysADLFactory.eINSTANCE.createAbstractComponentDef();
 		device.setName("DeviceCP");
-		device.getPorts().add(deviceOPT);
-		device.getPorts().add(sensorIPT);
-		device.getPorts().add(actuatorOPT);
-		device.getPorts().add(serviceIPT);
+		device.setLowerBound(1);
+		device.setUpperBound(-1);
 		device.getComposition().add(sensor);
 		device.getComposition().add(actuator);
 		device.getComposition().add(controller);
 		device.getComposition().add(gateway);
 		device.getComposition().add(datastore);
-		
+		AbstractPortUse deviceOPT = SysADLFactory.eINSTANCE.createAbstractPortUse();
+		deviceOPT.setName("DeviceOPT");
+		deviceOPT.setFlowProperty(FlowProperty.OUT);
+		deviceOPT.setLowerBound(0);
+		deviceOPT.setUpperBound(-1);
+		device.getPorts().add(deviceOPT);
+		AbstractPortUse sensorIPTd = SysADLFactory.eINSTANCE.createAbstractPortUse();
+		sensorIPTd.setName("SensorIPT");
+		sensorIPTd.setFlowProperty(FlowProperty.IN);
+		sensorIPTd.setLowerBound(0);
+		sensorIPTd.setUpperBound(-1);
+		device.getPorts().add(sensorIPTd);
+		AbstractPortUse actuatorOPTd = SysADLFactory.eINSTANCE.createAbstractPortUse();
+		actuatorOPTd.setName("ActuatorOPT");
+		actuatorOPTd.setFlowProperty(FlowProperty.OUT);
+		actuatorOPTd.setLowerBound(0);
+		actuatorOPTd.setUpperBound(-1);
+		device.getPorts().add(actuatorOPTd);
+		AbstractPortUse serviceIPTd = SysADLFactory.eINSTANCE.createAbstractPortUse();
+		serviceIPTd.setName("ServiceIPT");
+		serviceIPTd.setFlowProperty(FlowProperty.IN);
+		serviceIPTd.setLowerBound(0);
+		serviceIPTd.setUpperBound(-1);
+		device.getPorts().add(serviceIPTd);
+				
 		AbstractComponentDef IoTARCH = SysADLFactory.eINSTANCE.createAbstractComponentDef();
 		IoTARCH.setName("IoTARCH");
-		device.getComposition().add(sensor);
-		device.getComposition().add(actuator);
-		device.getComposition().add(device);
-		device.getComposition().add(gateway);
-		device.getComposition().add(datastore);
+		IoTARCH.getComposition().add(sensor);
+		IoTARCH.getComposition().add(actuator);
+		IoTARCH.getComposition().add(device);
+		IoTARCH.getComposition().add(gateway);
+		IoTARCH.getComposition().add(datastore);
 		
 		// connectors
 		AbstractConnectorDef sensorDataCN = SysADLFactory.eINSTANCE.createAbstractConnectorDef();
 		sensorDataCN.setName("SensorDataCN");
-		sensorDataCN.getPorts().add(sensorOPT);
-		sensorDataCN.getPorts().add(sensorIPT);
-		AbstractFlow f = SysADLFactory.eINSTANCE.createAbstractFlow();
-		f.setSource(sensorOPT);
-		f.setSource(sensorIPT);
-		sensorDataCN.getFlows().add(f);
+		AbstractPortUse sensorOPT_ = SysADLFactory.eINSTANCE.createAbstractPortUse();
+		sensorOPT_.setName("SensorOPT");
+		sensorOPT_.setFlowProperty(FlowProperty.OUT);
+		sensorDataCN.getPorts().add(sensorOPT_);
+		AbstractPortUse sensorIPT_ = SysADLFactory.eINSTANCE.createAbstractPortUse();
+		sensorIPT_.setName("SensorIPT");
+		sensorIPT_.setFlowProperty(FlowProperty.IN);
+		sensorDataCN.getPorts().add(sensorIPT_);
+		AbstractFlow f_sensorDataCN = SysADLFactory.eINSTANCE.createAbstractFlow();
+		f_sensorDataCN.setSource(sensorOPT_);
+		f_sensorDataCN.setTarget(sensorIPT_);
+		sensorDataCN.getFlows().add(f_sensorDataCN);
 		
 		AbstractConnectorDef actuatorCommandCN = SysADLFactory.eINSTANCE.createAbstractConnectorDef();
 		actuatorCommandCN.setName("ActuatorCommandCN");
-		sensorDataCN.getPorts().add(actuatorOPT);
-		sensorDataCN.getPorts().add(actuatorIPT);
-		f.setSource(actuatorOPT);
-		f.setSource(actuatorIPT);
-		sensorDataCN.getFlows().add(f);
+		AbstractPortUse actuatorOPT_ = SysADLFactory.eINSTANCE.createAbstractPortUse();
+		actuatorOPT_.setName("ActuatorOPT");
+		actuatorOPT_.setFlowProperty(FlowProperty.OUT);
+		actuatorCommandCN.getPorts().add(actuatorOPT_);
+		AbstractPortUse actuatorIPT_ = SysADLFactory.eINSTANCE.createAbstractPortUse();
+		actuatorIPT_.setName("ActuatorIPT");
+		actuatorIPT_.setFlowProperty(FlowProperty.IN);
+		actuatorCommandCN.getPorts().add(actuatorIPT_);
+		AbstractFlow f_actuatorCommandCN = SysADLFactory.eINSTANCE.createAbstractFlow();
+		f_actuatorCommandCN.setSource(actuatorOPT_);
+		f_actuatorCommandCN.setTarget(actuatorIPT_);
+		actuatorCommandCN.getFlows().add(f_actuatorCommandCN);
 		
 		AbstractConnectorDef deviceDataCN = SysADLFactory.eINSTANCE.createAbstractConnectorDef();
 		deviceDataCN.setName("DeviceDataCN");
-		sensorDataCN.getPorts().add(deviceOPT);
-		sensorDataCN.getPorts().add(deviceIPT);
-		f.setSource(deviceOPT);
-		f.setSource(deviceIPT);
-		sensorDataCN.getFlows().add(f);
+		AbstractPortUse deviceOPT_ = SysADLFactory.eINSTANCE.createAbstractPortUse();
+		deviceOPT_.setName("DeviceOPT");
+		deviceOPT_.setFlowProperty(FlowProperty.OUT);
+		deviceDataCN.getPorts().add(deviceOPT_);
+		AbstractPortUse deviceIPT_ = SysADLFactory.eINSTANCE.createAbstractPortUse();
+		deviceIPT_.setName("DeviceIPT");
+		deviceIPT_.setFlowProperty(FlowProperty.IN);
+		deviceDataCN.getPorts().add(deviceIPT_);
+		AbstractFlow f_deviceDataCN = SysADLFactory.eINSTANCE.createAbstractFlow();
+		f_deviceDataCN.setSource(deviceOPT_);
+		f_deviceDataCN.setTarget(deviceIPT_);
+		deviceDataCN.getFlows().add(f_deviceDataCN);
 		
 		AbstractConnectorDef datastoreCN = SysADLFactory.eINSTANCE.createAbstractConnectorDef();
 		datastoreCN.setName("DataStoreCN");
-		sensorDataCN.getPorts().add(dataOPT);
-		sensorDataCN.getPorts().add(dataIPT);
-		f.setSource(dataOPT);
-		f.setSource(dataIPT);
-		sensorDataCN.getFlows().add(f);
+		AbstractPortUse dataOPT_ = SysADLFactory.eINSTANCE.createAbstractPortUse();
+		dataOPT_.setName("DataOPT");
+		dataOPT_.setFlowProperty(FlowProperty.OUT);
+		datastoreCN.getPorts().add(dataOPT_);
+		AbstractPortUse dataIPT_ = SysADLFactory.eINSTANCE.createAbstractPortUse();
+		dataIPT_.setName("DataIPT");
+		dataIPT_.setFlowProperty(FlowProperty.IN);
+		datastoreCN.getPorts().add(dataIPT_);
+		AbstractFlow f_datastoreCN = SysADLFactory.eINSTANCE.createAbstractFlow();
+		f_datastoreCN.setSource(dataOPT_);
+		f_datastoreCN.setTarget(dataIPT_);
+		datastoreCN.getFlows().add(f_datastoreCN);
 		
 		AbstractConnectorDef serviceCN = SysADLFactory.eINSTANCE.createAbstractConnectorDef();
 		serviceCN.setName("ServiceCN");
-		sensorDataCN.getPorts().add(serviceOPT);
-		sensorDataCN.getPorts().add(serviceIPT);
-		f.setSource(serviceOPT);
-		f.setSource(serviceIPT);
-		sensorDataCN.getFlows().add(f);
+		AbstractPortUse serviceOPT_ = SysADLFactory.eINSTANCE.createAbstractPortUse();
+		serviceOPT_.setName("ServiceOPT");
+		serviceOPT_.setFlowProperty(FlowProperty.OUT);
+		serviceCN.getPorts().add(serviceOPT_);
+		AbstractPortUse serviceIPT_ = SysADLFactory.eINSTANCE.createAbstractPortUse();
+		serviceIPT_.setName("ServiceIPT");
+		serviceIPT_.setFlowProperty(FlowProperty.IN);
+		serviceCN.getPorts().add(serviceIPT_);
+		AbstractFlow f_serviceCN = SysADLFactory.eINSTANCE.createAbstractFlow();
+		f_serviceCN.setSource(serviceOPT_);
+		f_serviceCN.setTarget(serviceIPT_);
+		serviceCN.getFlows().add(f_serviceCN);
 		
 		// activities
 		AbstractActivityDef monitoring = SysADLFactory.eINSTANCE.createAbstractActivityDef();
@@ -227,6 +317,7 @@ public class StyleIoTBuilder extends ElementBuilder {
 		IoTStyle.getDefinitions().add(datastore);
 		IoTStyle.getDefinitions().add(controller);
 		IoTStyle.getDefinitions().add(gateway);
+		IoTStyle.getDefinitions().add(IoTARCH);
 		
 		IoTStyle.getDefinitions().add(sensorDataCN);
 		IoTStyle.getDefinitions().add(actuatorCommandCN);
@@ -253,7 +344,6 @@ public class StyleIoTBuilder extends ElementBuilder {
 		IoTStyle.getInvariants().add(checkActuatorCommandCN);
 		IoTStyle.getInvariants().add(checkServiceCN);
 		IoTStyle.getInvariants().add(checkDeviceDataCN);
-		
 		IoTStyle.getInvariants().add(ControllerMustBeEmbeddedInDevice);
 		IoTStyle.getInvariants().add(SensorMustBeConnectedToDeviceOrController);
 		IoTStyle.getInvariants().add(ActuatorMustBeConnectedToDeviceOrController);
